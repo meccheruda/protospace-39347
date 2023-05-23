3cbbce04fd94d7127738a3783803f08639633ccf
meccheruda <masanao_f@yahoo.co.jp>
2023-05-23 14:01:18 +0900

    詳細ページの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @prototype = @user.prototypes
+    @user_prototypes = current_user.prototypes
   end
 
   private
